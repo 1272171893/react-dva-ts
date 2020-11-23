f50fbfa70e57b6f92d9f01640ead13df839557ca
1272171893 <1272171893@qq.com>
2020-11-23 17:58:00 +0800

    添加全局store

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import routers from "routers/index";
+console.log(routers);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App /> 
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
---
+++ src/stores/global.tsx
@@ -0,0 +1,12 @@
+const state = {};
+const reducers = {};
+const effects = {};
+const subscriptions = {};
+
+export default {
+  namespace: "global",
+  state: state,
+  reducers: reducers,
+  effects: effects,
+  subscriptions: subscriptions,
+};
