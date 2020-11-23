8d9e4f797305619a70b96f9c11bd83a4fb2c1022
1272171893 <1272171893@qq.com>
2020-11-23 20:58:38 +0800

    路由守卫

--- src/stores/global.tsx
+++ src/stores/global.tsx
@@ -1,7 +1,18 @@
+import { History } from "history";
+type Isetup = {
+  dispatch: any;
+  history: History;
+};
 const state = {};
 const reducers = {};
 const effects = {};
-const subscriptions = {};
+const subscriptions = {
+  setup({ dispatch, history }: Isetup) {
+    history.listen((location, value) => {
+      console.log(location, value);
+    });
+  },
+};
 
 export default {
   namespace: "global",
