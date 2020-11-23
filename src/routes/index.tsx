dbc609de26301b4b71bc95732a1108d894f3fa6e
1272171893 <1272171893@qq.com>
2020-11-23 19:49:46 +0800

    申明

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,11 +3,10 @@ import PropTypes from "prop-types";
 import { Switch, Route, routerRedux } from "dva/router";
 import dynamic from "dva/dynamic";
 import models from "routes/models";
-
+import { IProps } from "types/routers";
 const { ConnectedRouter } = routerRedux;
-
-const Routers = ({ history, app }: any) => {
-  console.log('history',history,models)
+const Routers = ({ history, app }: IProps) => {
+  console.log("history", history, models);
   return (
     <ConnectedRouter history={history}>
       <Switch>
--- src/types/routers.d.ts
+++ src/types/routers.d.ts
@@ -1,3 +1,14 @@
 import { History } from "history";
 
-type DvaApp = {};
+type DvaApp = {
+  _models: any;
+  _store: any;
+  _plugin: any;
+  use: (...args: any[]) => any;
+  model: any;
+  start: any;
+};
+export type IProps = {
+  history: History;
+  app: DvaApp | any;
+};
