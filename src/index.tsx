a22aeaf6c0c65cc82079329a9c8a314dd0bbb242
1272171893 <1272171893@qq.com>
2020-11-24 10:47:36 +0800

    添加资源

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,9 @@
 import dva from "dva";
 import { createBrowserHistory } from "history";
+import "assets/styles/normalize.less";
+import "assets/styles/common.less";
+import "assets/iconfonts/iconfont.css";
+import "antd/dist/antd.css"
 const app = dva({
   history: createBrowserHistory(),
 });
