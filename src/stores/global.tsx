a38e0f1e9eb96914c187246641c2da0de214f4cf
1272171893 <1272171893@qq.com>
2020-11-24 10:10:32 +0800

    路由守卫

--- src/stores/global.tsx
+++ src/stores/global.tsx
@@ -3,12 +3,20 @@ type Isetup = {
   dispatch: any;
   history: History;
 };
-const state = {};
+const state = {
+  exclude: ["/", "/login"],
+};
 const reducers = {};
 const effects = {};
 const subscriptions = {
   setup({ dispatch, history }: Isetup) {
     history.listen((location, value) => {
+      const token: string = window.sessionStorage.getItem("token") || "";
+      const pathname: string = location.pathname || "";
+      if (!state.exclude.includes(pathname) && token === "") {
+        history.push("/login");
+        return;
+      }
       console.log(location, value);
     });
   },
