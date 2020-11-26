5db992c1a24653972f4fcfc8b7089570494602a4
1272171893 <1272171893@qq.com>
2020-11-26 17:24:09 +0800

    添加包

--- src/components/layouts/HeaderLayout.tsx
+++ src/components/layouts/HeaderLayout.tsx
@@ -1,7 +1,8 @@
 import React, { FC } from "react";
+import "components/layouts/less/HeaderLayout.less";
 import { connect } from "dva";
 const HeaderLayout: FC = (props) => {
   console.log(props);
-  return <div>我是头部</div>;
+  return <div className="w100 relative">我是头部</div>;
 };
 export default connect(({ global }: any) => ({ global }))(HeaderLayout);
