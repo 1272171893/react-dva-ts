309d2d100d671c6961ef22d8a8b64a4a6ad36128
1272171893 <1272171893@qq.com>
2020-11-27 12:46:23 +0800

    头部搭建

--- src/components/layouts/HeaderLayout.tsx
+++ src/components/layouts/HeaderLayout.tsx
@@ -1,8 +1,18 @@
 import React, { FC } from "react";
 import "components/layouts/less/HeaderLayout.less";
 import { connect } from "dva";
-const HeaderLayout: FC = (props) => {
-  console.log(props);
-  return <div className="header_box w100 absolute t_l_0">我是头部</div>;
+const HeaderLayout: FC<any> = (props) => {
+  const { global: state } = props;
+  console.log(props, state);
+  return (
+    <div className="header_box w100 absolute t_l_0 flex_nowrap j_b">
+      <div className="header_left h100 flex_nowrap a_c">
+        <img src={state.logo} alt="登录logo" />
+        <h1>{state.title}</h1>
+      </div>
+      <div className="header_main h100 flex_nowrap a_c flex1">2</div>
+      <div className="header_right h100 flex_nowrap a_c">3</div>
+    </div>
+  );
 };
 export default connect(({ global }: any) => ({ global }))(HeaderLayout);
--- src/stores/global.tsx
+++ src/stores/global.tsx
@@ -5,6 +5,8 @@ type Isetup = {
 };
 const state = {
   exclude: ["/", "/login"],
+  logo: require("assets/images/bg/logo.jpg"),
+  title: "智慧分享平台",
 };
 const reducers = {};
 const effects = {};
