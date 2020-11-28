d0fd3582444ed7ee341ef2d20c75de0e26497977
1272171893 <1272171893@qq.com>
2020-11-28 17:13:33 +0800

    头部优化

--- src/components/layouts/HeaderLayout.tsx
+++ src/components/layouts/HeaderLayout.tsx
@@ -1,10 +1,11 @@
-import React, { FC, ReactElement } from "react";
+import React, { FC, useState, ReactElement } from "react";
 import "components/layouts/less/HeaderLayout.less";
 import { connect } from "dva";
 import { Avatar, Dropdown, Menu } from "antd";
 const { Item } = Menu;
 const HeaderLayout: FC<any> = (props) => {
   const { global: state } = props;
+  const [current, setCurrent] = useState<string>("1");
   console.log(props, state);
   const options: ReactElement = (
     <Menu>
@@ -12,13 +13,31 @@ const HeaderLayout: FC<any> = (props) => {
       <Item key="2">系统设置</Item>
     </Menu>
   );
+  const routerToPoint = (value: any) => {
+    setCurrent(value.key);
+  };
   return (
     <div className="header_box w100 absolute t_l_0 flex_nowrap j_b">
       <div className="header_left h100 flex_nowrap a_c">
         <img src={state.logo} alt="登录logo" />
         <h1>{state.title}</h1>
       </div>
-      <div className="header_main h100 flex_nowrap a_c flex1">2</div>
+      <div className="header_main h100 flex_nowrap a_c flex1">
+        <Menu
+          onClick={routerToPoint}
+          selectedKeys={[current]}
+          mode="horizontal"
+          className="menu_box"
+        >
+          <Item key="1">horizontal1</Item>
+          <Item key="2">horizontal2</Item>
+          <Item key="3">horizontal3</Item>
+          <Item key="4">horizontal4</Item>
+          <Item key="5">horizontal5</Item>
+          <Item key="6">horizontal6</Item>
+          <Item key="7">horizontal7</Item>
+        </Menu>
+      </div>
       <div className="header_right h100 flex_nowrap a_c">
         <div className="user_info flex1 pointer">
           <Dropdown overlay={options} placement="bottomCenter" arrow>
