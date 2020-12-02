1728eeaf693b784551772a5fa582c20b8d254d00
1272171893 <1272171893@qq.com>
2020-12-02 11:22:36 +0800

    登录优化

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,16 @@
 import React, { FC } from "react";
 import "pages/login/index.less";
 import { Layout } from "antd";
+import { connect } from "dva";
 const { Header, Footer, Sider, Content } = Layout;
-const App: FC = () => {
+const Login: FC<any> = (props) => {
+  const { login: state } = props;
+  console.log("props", props);
   return (
     <div className="wh100 login_box relative background_contain">
       <div className="top_center login_from inline_block">
         <Layout>
-          <Header>Header</Header>
+          <Header className='login_title t_c'>{state.loginTitle}</Header>
           <Content>Content</Content>
           <Footer>Footer</Footer>
         </Layout>
@@ -15,4 +18,4 @@ const App: FC = () => {
     </div>
   );
 };
-export default App;
+export default connect(({ login }: any) => ({ login }))(Login);
