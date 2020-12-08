a3f60f0aa60b442d48bbdd3aefe94eaf8001efcd
1272171893 <1272171893@qq.com>
2020-12-08 20:46:21 +0800

    请求接口

--- src/mocks/index.js
+++ src/mocks/index.js
@@ -8,9 +8,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.post("/user/login", (req, res) => {
   console.log("req", req.body);
-  // const username = req.body.username || ""
-  // const password = req.body.password || ""
-  res.send({ data: "hello world" });
+  const username = req.body.username || "";
+  const password = req.body.password || "";
+  if (username === "admin" && password === "admin") {
+    const data = {
+      userId: new Date().getTime(),
+      userName: "超级管理员",
+      token: new Date().getTime(),
+    };
+    res.send({ code: 200, data: data, meaasge: "登陆成功" });
+    return;
+  }
+  res.send({ code: 0, data: null, meaasge: "登陆失败" });
 });
 console.log(1111);
 app.listen(9000);
